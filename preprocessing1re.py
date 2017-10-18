5e9c8117eea3fb66a66f030a979e022e9cbd2021
springwang93 <26215069+springwang93@users.noreply.github.com>
2017-10-18 17:57:22 +0900

    Update preprocessing1re.py

--- preprocessing1re.py
+++ preprocessing1re.py
@@ -10,11 +10,11 @@ f1=open('C:/Users/WANGSITONG/Desktop/research/female1.txt','w')
 
 
 for line in f:
-    regex=re.compile('[^/a-zA-Z0-9 \t]')
+    regex=re.compile('[^/a-zA-Z0-9 \t]') #\t tab
     result=regex.sub('',line)
     
     result = result.replace("\t", " ")
-    f1.write(result+"\n")
+    f1.write(result+"\n") #\n space 
     print(result+"\n")
 
 f1.close()
